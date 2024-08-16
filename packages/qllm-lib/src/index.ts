026189452c6b68b92d41e18839d572e07d04c131
yatchiya <yarab@gmail.com>
2024-08-16 15:01:15 +0200

    clean

--- packages/qllm-lib/src/index.ts
+++ packages/qllm-lib/src/index.ts
@@ -1,4 +1,4 @@
-export * from "./core/providers/types";
+
 export * from "./config/provider_config";
 export * from "./core/providers/provider_factory";
 export * from './config/configuration_manager';
