6bc9d88388d4a822e72b02288dff01eab2e80d28
Raphael MANSUY <raphael.mansuy.partner@decathlon.com>
2024-08-21 20:05:20 +0800

    update

--- packages/qllm-lib/src/demo.ts
+++ packages/qllm-lib/src/demo.ts
@@ -1,5 +1,5 @@
-import { getEmbeddingProvider, getLLMProvider } from "./providers";
-import { EmbeddingProvider, LLMProvider } from "./types";
+import { getEmbeddingProvider, getLLMProvider } from './providers';
+import { EmbeddingProvider, LLMProvider } from './types';
 
 const demo = async () => {
   console.log('Test demo');
@@ -17,6 +17,8 @@ const demo = async () => {
   await stream(provider);
 
   await embedding(embedddingProvider);
+
+  await completionImage(provider);
 };
 
 demo()
@@ -67,6 +69,34 @@ async function stream(provider: LLMProvider) {
   for await (const message of result) {
     process.stdout.write(message.text || '');
   }
+}
+
+async function completionImage(provider: LLMProvider) {
+  const result = await provider.generateChatCompletion({
+    messages: [
+      {
+        role: 'user',
+        content: [
+          {
+            type: 'text',
+            text: 'Can you describe this image?',
+          },
+          {
+            type: 'image_url',
+            imageUrl: {
+              url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Caucasus%2C_Ingushetia%2C_%D0%98%D0%BD%D0%B3%D1%83%D1%88%D1%81%D0%BA%D0%B8%D0%B5_%D0%B1%D0%BE%D0%B5%D0%B2%D1%8B%D0%B5_%D0%B8_%D1%81%D0%BC%D0%BE%D1%82%D1%80%D0%BE%D0%B2%D1%8B%D0%B5_%D0%B1%D0%B0%D1%88%D0%BD%D0%B8%2C_%D0%B3%D0%BE%D1%80%D1%8B_%D0%9A%D0%B0%D0%B2%D0%BA%D0%B0%D0%B7%D0%B0.jpg/2560px-Caucasus%2C_Ingushetia%2C_%D0%98%D0%BD%D0%B3%D1%83%D1%88%D1%81%D0%BA%D0%B8%D0%B5_%D0%B1%D0%BE%D0%B5%D0%B2%D1%8B%D0%B5_%D0%B8_%D1%81%D0%BC%D0%BE%D1%82%D1%80%D0%BE%D0%B2%D1%8B%D0%B5_%D0%B1%D0%B0%D1%88%D0%BD%D0%B8%2C_%D0%B3%D0%BE%D1%80%D1%8B_%D0%9A%D0%B0%D0%B2%D0%BA%D0%B0%D0%B7%D0%B0.jpg',
+            },
+          },
+        ],
+      },
+    ],
+    options: {
+      model: 'gpt-4o-mini',
+      maxTokens: 1024,
+    },
+  });
+
+  console.log('result:', result);
 }
 
 async function embedding(provider: EmbeddingProvider) {
