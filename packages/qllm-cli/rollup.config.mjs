6d78ee07c4d0f43d0b3abbc490051c475b21fb4b
Raphael MANSUY <raphael.mansuy.partner@decathlon.com>
2024-08-29 13:50:45 +0800

    fix rollup

---
+++ packages/qllm-cli/rollup.config.mjs
@@ -0,0 +1,45 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+//import { terser } from 'rollup-plugin-terser';
+//import analyze from 'rollup-plugin-analyzer';
+//import gzip from 'rollup-plugin-gzip';
+
+const isProduction = false || process.env.NODE_ENV === 'production';
+
+export default {
+  input: 'dist/qllm.js', // Change this to point to the compiled TypeScript output
+  output: [
+    {
+      dir: 'dist',
+      format: 'cjs',
+      sourcemap: true,
+      exports: 'named'
+    },
+  ],
+  plugins: [
+    resolve({
+      preferBuiltins: true,
+    }),
+    commonjs(),
+    json(),
+   // isProduction && terser(),
+   // analyze({ summaryOnly: true }),
+   // isProduction && gzip(),
+  ],
+  external: [
+    'openai',
+    'groq-sdk',
+    '@anthropic-ai/sdk',
+    '@anthropic-ai/bedrock-sdk',
+    '@aws-sdk/client-bedrock',
+    '@aws-sdk/credential-providers',
+    'axios',
+    'js-yaml',
+    'mime-types',
+    'ollama',
+    'sqlite',
+    'uuid',
+    'zod',
+  ],
+};
