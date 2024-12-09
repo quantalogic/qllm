1be0fc2bcfee0164f1e6d4195d3a6ac15a47d999
Yatchi <35928424+YatchiYa@users.noreply.github.com>
2024-12-09 14:10:09 +0100

    AGENT v1 (#77)
    * add a first process workflow templating
    
    * add a first process workflow templating
    
    * update anthropic models + haiku 3.5
    
    * add a small feature to be able to parse a file and add it to a content prompt, it s a first version that will need to improve by parsing the document loader and all
    
    * add a small feature to be able to parse a file and add it to a content prompt, it s a first version that will need to improve by parsing the document loader and all
    
    * add a small feature to be able to parse a file and add it to a content prompt, it s a first version that will need to improve by parsing the document loader and all
    
    * add a small feature to calculate prices tokens costs + example how to use
    
    * Add pdf parser and registry Parsers
    
    * Update error Handling in loadFromFile
    
    * Add validate content before parsing
    
    * Remove security check
    
    * Update loadFromUrl
    
    * Add test files
    
    * up new version workflow
    
    * debug linting error with eslint package
    
    * rm old test file
    
    * up new version workflow
    
    * up new version workflow
    
    * up new version workflow
    
    * up new version workflow
    
    * debug pdf parse
    
    * rm comments
    
    * up new version workflow
    
    * up new version workflow
    
    * add features tools
    
    * add features tools
    
    * add features tools
    
    * resolve conflict
    
    * solve conflicts tests
    
    * solve conflicts tests
    
    * add step names / desc
    
    * First version to load a file on s3
    
    * Update s3 tool
    
    * fix length file
    
    * Update s3 tool
    
    * Remove old s3 tool
    
    * Update workflow manager and executor for s3tool
    
    * add s3 tool to index
    
    * Add s3 tool to workflow to write a story and update demo test file
    
    * add test for analyze code
    
    * solve conflicts
    
    * debug document loader in template executor
    
    * fix bug crypto due to node version
    
    * 1st version of jira tool create/get ready
    
    * update packages
    
    * update s3 tool to save locally before save on s3
    
    * first set agent
    
    * first set agent
    
    * first set agent
    
    * first set agent
    
    * first set agent
    
    * try solve conflicts
    
    * try solve conflicts
    
    * try solve conflicts
    
    ---------
    
    Co-authored-by: yatchiya <yarab@gmail.com>
    Co-authored-by: jluong <jluong@novagen.tech>
    Co-authored-by: jluongg <158718486+jluongg@users.noreply.github.com>

--- packages/qllm-lib/src/tools/index.ts
+++ packages/qllm-lib/src/tools/index.ts
@@ -3,13 +3,5 @@ export * from "./github-loader";
 export * from "./file-saver.tool"; 
 export * from "./local-project-loader"; 
 export * from "./s3.tool";
-<<<<<<< HEAD
-<<<<<<< HEAD
-export * from "./jira.tools"
-=======
-export * from "./fileoverview-rag";
->>>>>>> 7485188 (Feat/rag tool workflow (#78))
-=======
 export * from "./fileoverview-rag";
 export * from "./jira.tools"
->>>>>>> 998ef47 (Intragting jira tool to workflow, remains to add getDefinition)
