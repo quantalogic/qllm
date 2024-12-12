4818062941f553f1c3164126a9ef5de8afd87268
jluong <jluong@novagen.tech>
2024-12-12 17:23:39 +0100

    resolve conflicts with master

--- packages/qllm-lib/src/tools/index.ts
+++ packages/qllm-lib/src/tools/index.ts
@@ -1,7 +1,10 @@
+import { S3ToLocalTool } from "./s3_to_local.tool";
+
 export * from "./base-tool";
 export * from "./github-loader";
 export * from "./file-saver.tool"; 
 export * from "./local-project-loader"; 
 export * from "./s3.tool";
 export * from "./fileoverview-rag";
-export * from "./jira.tools";
+export * from "./jira.tool";
+export * from "./s3_to_local.tool";
