a9ebe3a76cc8006b6ef1d38be0e8d0573061301f
jluong <jluong@novagen.tech>
2024-12-13 10:47:00 +0100

    update s3 env variables

--- packages/qllm-lib/src/tools/jira.tool.ts
+++ packages/qllm-lib/src/tools/jira.tool.ts
@@ -1,5 +1,8 @@
 import { BaseTool, ToolDefinition } from "./base-tool";
 import { Version3Client } from "jira.js";
+import path from 'path';
+dotenv.config();
+
 
 export interface JiraInput {
   /** Operation to perform */
